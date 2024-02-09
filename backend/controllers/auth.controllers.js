eab7b1583c0aa8f6a7d151cab3e2d9080769435b
chennakesava <110022346+chenna319@users.noreply.github.com>
2024-02-09 15:54:33 +0530

    first part is backend

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,86 @@
+import bcyrpt from "bcryptjs";
+import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(403).json({ error: "password is not matching" });
+    }
+
+    const user = await User.findOne({ username });
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    //hash password
+    const hashedPassword = await bcyrpt.hash(password, 10);
+    //profile picture
+
+    const boyPorfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlPorfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyPorfilePic : girlPorfilePic,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in SignUp Controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordMatch = await bcyrpt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordMatch) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+    res.status(201).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in Login Controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const logout = (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (err) {
+    console.log("Error in Logout Controller", err.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import AuthRoute from "./routes/auth.routes.js";
+import MessageRoute from './routes/message.routes.js'
+import userRoutes from './routes/user.routes.js'
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import dotenv from "dotenv";
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+//controllers
+app.use(express.json()) // to parse the incoming requests with json payloads (from req.body)
+app.use(cookieParser());
+
+
+app.use("/api/auth", AuthRoute);
+app.use("/api/messages",MessageRoute);
+app.use("/api/users",userRoutes);
+app.use(express.json())
+
+
+app.listen(PORT, () => {
+    connectToMongoDB()
+  console.log(`server running at ${PORT}`);
+});
